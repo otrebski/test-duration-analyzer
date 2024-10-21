82fb7e1e74f4e01f8e41e7d433ad2387a2d53f6b
K <krzysztof.otrebski@gmail.com>
2024-10-21 23:31:57 +0200

    Refactor to idiomatic rust

--- src/parser.rs
+++ src/parser.rs
@@ -3,18 +3,10 @@ use std::fs;
 use quick_xml::de::from_str;
 
 pub fn file_to_report(path: &FilePath) -> Option<TestSuite> {
-    let result = match fs::read_to_string(&path.path) {
-        Ok(content) => match from_str::<TestSuite>(&content) {
-            Ok(test_suite) => Some(test_suite),
-            Err(_e) => {
-                eprintln!("Can't parse file {}", &path.path);
-                None
-            }
-        },
-        Err(_e) => {
-            eprintln!("Can't read content of {}", &path.path);
-            None
-        }
-    };
-    result
+    let content = fs::read_to_string(&path.path)
+        .map_err(|_| { eprintln!("Can't read content of file {}", path.path); })
+        .ok()?;
+    from_str::<TestSuite>(&content)
+        .map_err(|_| { eprintln!("Can't parse file {}", path.path) })
+        .ok()
 }
